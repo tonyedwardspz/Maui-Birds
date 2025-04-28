d607d21d520f218c755c19d3f02672b1f78e6727
Tony Edwards <tonyedwardspz@users.noreply.github.com>
2025-04-28 20:45:25 +0100

    Adds wheel of feathers view

---
+++ Views/WheelOfFeathersView.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_Birds.Views;
+
+public partial class WheelOfFeathersView : ContentPage
+{
+    public WheelOfFeathersView()
+    {
+        InitializeComponent();
+    }
+}
